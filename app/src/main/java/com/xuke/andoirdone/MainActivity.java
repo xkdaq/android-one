f24587dd6fccb0125da05677ddc5aedc4561a7f1
xuke <ke.xu@cymobi.com>
2018-01-22 16:42:51 +0800

    配置三方库

--- app/src/main/java/com/xuke/andoirdone/MainActivity.java
+++ app/src/main/java/com/xuke/andoirdone/MainActivity.java
@@ -1,13 +1,23 @@
 package com.xuke.andoirdone;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.tv)
+    TextView tv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+        tv.setText("许可");
     }
 }
